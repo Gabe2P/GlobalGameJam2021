3de15c1537cbeaf05c2f408be5914ef676e30065
DESKTOP-6HTUSTO\scout <anthonyeckman@gmail.com>
2021-01-29 23:23:39 -0600

    BananaTime

--- Assets/Scripts/RequestManager.cs
+++ Assets/Scripts/RequestManager.cs
@@ -2,29 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "CreatItem/NewItem")]
-public class BaseItem : ScriptableObject
+public class RequestManager : MonoBehaviour
 {
-
-    public string Name;
-
-    public float Mass;
-    public float Drag;
-
-    public Sprite sprite;
-
-
-
-
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
