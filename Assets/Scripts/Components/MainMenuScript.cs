fdce05b36fd154a3ed747fcbbbe777c482112cb4
Gabriel Tupy <gabetupy@gmail.com>
2021-01-31 14:19:28 -0600

    Fixed more shit

---
+++ Assets/Scripts/Components/MainMenuScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public string nextScene = "GGJ";
+
+    public void ChangeScene()
+    {
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.ChangeScene(nextScene);
+        }
+    }
+}
